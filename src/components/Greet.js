d97f6db24f8df4bb74b81cef55b5538f0cb6a864
Erioro Paul <erioroegbelughepaul@gmail.com>
2023-11-29 15:21:55 +0100

    new

---
+++ src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// function Greet(){
+//     return(
+//         <h1>
+//             Hello Paul
+//         </h1>
+//     )
+// }
+
+let Greet = () => <h1>Hello Me</h1>
+export default Greet
